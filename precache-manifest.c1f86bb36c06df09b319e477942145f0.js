0780101b041741f9096df86b53bcce69094bf114
asadmehdii <66443072+asadmehdii@users.noreply.github.com>
2022-09-17 21:45:45 +0500

    Updates

---
+++ precache-manifest.c1f86bb36c06df09b319e477942145f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95864b570f60fcfa1f5145dbfe320e21",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/index.html"
+  },
+  {
+    "revision": "40aae883ce70ab5eabe1",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/static/css/main.838a69de.chunk.css"
+  },
+  {
+    "revision": "5f341696aff6f5d616bd",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/static/js/2.2a4154e7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/static/js/2.2a4154e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40aae883ce70ab5eabe1",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/static/js/main.439663de.chunk.js"
+  },
+  {
+    "revision": "a20586328cf13ddd6ae1",
+    "url": "/%E2%80%9Chttp://asadmehdii.github.io/react-tabel/static/js/runtime-main.b4064227.js"
+  }
+]);
