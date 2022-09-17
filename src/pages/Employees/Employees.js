86b34c330a26d2010dbb13d8eb3b58dd4ea5e52e
asadmehdii <66443072+asadmehdii@users.noreply.github.com>
2022-09-17 21:48:18 +0500

    deployed

---
+++ src/pages/Employees/Employees.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import EmployeeForm from "./EmployeeForm";
+import PageHeader from "../../components/PageHeader";
+import PeopleOutlineTwoToneIcon from "@material-ui/icons/PeopleOutlineTwoTone";
+import {
+  Paper,
+  makeStyles,
+  TableBody,
+  TableRow,
+  TableCell,
+  Toolbar,
+  InputAdornment,
+} from "@material-ui/core";
+import useTable from "../../components/useTable";
+import * as employeeService from "../../services/employeeService";
+import Controls from "../../components/controls/Controls";
+import { Search } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  pageContent: {
+    margin: theme.spacing(5),
+    padding: theme.spacing(3),
+  },
+  searchInput: {
+    width: "75%",
+  },
+}));
+
+const headCells = [
+  { id: "fullName", label: "id" },
+  { id: "email", label: "Name" },
+  { id: "mobile", label: "Age" },
+
+  { id: "department", label: "City", disableSorting: true },
+];
+
+export default function Employees() {
+  const classes = useStyles();
+  const [records, setRecords] = useState(employeeService.getAllEmployees());
+  const [filterFn, setFilterFn] = useState({
+    fn: (items) => {
+      return items;
+    },
+  });
+
+  const { TblContainer, TblHead, TblPagination, recordsAfterPagingAndSorting } =
+    useTable(records, headCells, filterFn);
+
+  const handleSearch = (e) => {
+    let target = e.target;
+    setFilterFn({
+      fn: (items) => {
+        if (target.value == "") return items;
+        else
+          return items.filter((x) =>
+            x.fullName.toLowerCase().includes(target.value)
+          );
+      },
+    });
+  };
+
+  return (
+    <>
+      <PageHeader
+        title="Tabel"
+        subTitle="Form design with validation"
+        icon={<PeopleOutlineTwoToneIcon fontSize="large" />}
+      />
+      <Paper className={classes.pageContent}>
+        {/* <EmployeeForm /> */}
+        <Toolbar>
+          <Controls.Input
+            label="Search Tabel"
+            className={classes.searchInput}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+            onChange={handleSearch}
+          />
+        </Toolbar>
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {recordsAfterPagingAndSorting().map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+        <TblPagination />
+      </Paper>
+    </>
+  );
+}
